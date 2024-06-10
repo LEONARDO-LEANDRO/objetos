47952a32ffe0e50c6be53e9ce3be22cf2bb4ed4c
LEONARDO SOUZA LEANDRO <128157527+LEONARDO-LEANDRO@users.noreply.github.com>
2024-06-10 14:00:13 -0300

    Add files via upload

---
+++ aula2/aninhados.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome: 'Leonardo',
+    idade: 18,
+    email: 'leonardo.souza@escola.pr.gov.br',
+    telefone: ['(43) 999663630)", "(43) 999663630'],
+
+};
+
+cliente.endereço = {
+    rua:'R. Maria Inez Lourenço',
+    numero: 3,
+    apartamento: false,
+    complemento: 'Do lado da APAE',
+
+};
+
+console.log(cliente);
--- aula2/tipos.js
+++ aula2/tipos.js
@@ -2,7 +2,7 @@ const cliente = {
     nome:"Leonardo",
     idade:18,
     email:"3135432134655",
-    telefone:"6543247454",
+    telefone:["6543247454","3246575646"],
 
 };
-console.log(cliente);
+console.log(cliente.telefone)
